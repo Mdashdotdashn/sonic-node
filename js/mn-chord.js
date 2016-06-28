4a9113acb64cf9e365429f17015615d8ba8b68fc
marc.nostromo <marc.nostromo@gmail.com>
2016-06-28 19:51:34 +0200

    added chord inversion

--- js/mn-chord.js
+++ js/mn-chord.js
@@ -23,3 +23,36 @@ Chord.prototype.distanceFrom = function(fromChord)
 {
   return note_gravity_center(this.notes_) - note_gravity_center(fromChord.notes_); 
 }
+
+Chord.prototype.invertUp = function()
+{
+  this.notes_.sort();
+  var lowest = this.notes_.shift();
+  this.notes_.push(lowest+12);
+}
+
+Chord.prototype.invertDown = function()
+{
+  this.notes_.sort();
+  var highest = this.notes_.pop();
+  this.notes_.unshift(highest-12);
+}
+
+Chord.prototype.invert = function(distance)
+{
+  var sign = Math.sign(distance);
+  var count = Math.abs(distance);
+
+  for (var i = 0; i< count; i++)
+  {
+    switch(sign)
+    {
+      case 1:
+        this.invertUp();
+        break;
+
+      case -1:
+        this.invertDown();
+    }
+  }
+}

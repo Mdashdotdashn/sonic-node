8efbdc62f0c28dc3967dfd5b33c5eaf9111a5c3a
marc.nostromo <marc.nostromo@gmail.com>
2016-06-28 21:18:38 +0200

    added progression rectification

--- test.js
+++ test.js
@@ -16,6 +16,7 @@ var rootNote = "c4"
 var scale = "minor"
 var progression = [1, 7 ,4 , 6, 1, 3, 6, 6];
 
+
 var analyseProgression = function(sequence)
 {
   console.log("chord sequence = " + JSON.stringify(sequence));
@@ -34,6 +35,58 @@ var analyseProgression = function(sequence)
   console.log("entropy = " + sum);
 }
 
+function rectify_closest(from, to)
+{
+  var d = to.distanceFrom(from);
+  var sign = Math.sign(d);
+  d = Math.abs(d);
+  var mind = 1000;
+
+  while(mind > d)
+  {
+    mind =d;
+    to.invert(-sign);
+    d = Math.abs(to.distanceFrom(from));
+  }
+  to.invert(sign);
+  return to;
+}
+
+function rectify_progression_sequential(sequence)
+{
+  for (var i = 0; i < sequence.length-1; i++)
+  {
+    sequence[i+1] = rectify_closest(sequence[i],sequence[i+1]);
+  }
+}
+
+function rectify_progression_to_first(sequence)
+{
+  for (var i = 0; i < sequence.length-1; i++)
+  {
+    sequence[i+1] = rectify_closest(sequence[0],sequence[i+1]);
+  }
+}
+
+function rectify_progression(sequence)
+{
+  var leftIndex = 1;
+  var rightIndex = sequence.length -1;
+
+  sequence[rightIndex--] = rectify_closest(sequence[0], sequence[rightIndex]);
+
+  while (leftIndex < rightIndex)
+  {
+    sequence[leftIndex] = rectify_closest(sequence[leftIndex -1], sequence[leftIndex]);
+    leftIndex++;
+    if (rightIndex > leftIndex)
+    {
+      sequence[rightIndex] = rectify_closest(sequence[rightIndex+1], sequence[rightIndex]);
+      rightIndex--;
+    }
+  }
+}
+
 var device = MidiDevice.find("circuit");
 
 var cp = new ChordProgression(rootNote, scale);
@@ -45,9 +98,12 @@ progression.forEach(function(degree) {
   chordSequence.push(cp.chord(degree));
   });
 
+//chordSequence[0].invert(-2);
+
+rectify_progression_to_first(chordSequence);
+
 analyseProgression(chordSequence);
 
-/*
 sequence.setContent(chordSequence);
 
 heartbeat.connect(sequence);
@@ -69,4 +125,4 @@ heartbeat.connect(sequence);
   });
 
 heartbeat.run();
-*/
+

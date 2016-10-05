709d650cb443b1a9bdca0c2cbfef786d076f3b17
mre <mre@ableton.com>
2016-10-05 11:44:37 +0200

    initial test file for scale detection

---
+++ test/scale-detection.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+
+require("../js/mn-chord.js");
+require("../js/mn-note.js");
+require("../js/mn-scale.js");
+
+var intervals = scale("c4","major");
+console.log(intervals);

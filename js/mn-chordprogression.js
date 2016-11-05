1e63f62c77f42b481d44759768f20b1df60e3a3e
mre <mre@ableton.com>
2016-11-05 14:13:16 +0100

    Re-organise chord progression code

--- js/mn-chordprogression.js
+++ js/mn-chordprogression.js
@@ -1,19 +1,51 @@
 require("./mn-chord.js");
 require("./mn-scale.js");
 
-ChordProgression = function(noteName, scaleName)
+// Chord progression helper object
+
+ChordProgression = function(rootNote, mode)
 {
-  this.scaleNotes_ = scale(noteName, scaleName);
+  this.scaleNotes_ = scale(rootNote, mode);
   this.scaleNotes_.forEach(function(note)
     {
       this.scaleNotes_.push(note+12);
     }, this);
 }
 
+// return the chord corresponding to the nth degree in the current progression
+// n starts with 1
+
 ChordProgression.prototype.chord = function(degree)
 {
   var n = this.scaleNotes_;
   return new Chord([n[degree-1], n[degree+1], n[degree+3]], n[degree-1]-24);
+}
+
+// creates a chord progression from a list of scale degree
+
+makeChordProgression = function(rootNote, mode, progression)
+{
+    var cp = new ChordProgression(rootNote, mode);
+    var chordSequence = [];
+
+    progression.forEach(function(degree) {
+      chordSequence.push(cp.chord(degree));
+      });
+
+    return chordSequence;
+}
+
+// creates a chord progression from a list chord names
+
+makeChordSequence = function(chordNames)
+{
+    var chordSequence = [];
+    chordNames.forEach(function(chordName)
+    {
+        chordSequence.push(new Chord(notesfromchordname(chordName)));
+    });
+
+    return chordSequence;
 }
 
 //------------------------------------------------------------
@@ -87,26 +119,4 @@ rectify_progression = function(sequence, mode)
       rectify_progression_inwards(sequence);
       break;
   }
-}
-
-makeChordProgression = function(rootNote, scale, progression)
-{
-    var cp = new ChordProgression(rootNote, scale);
-    var chordSequence = [];
-    progression.forEach(function(degree) {
-      chordSequence.push(cp.chord(degree));
-      });
-
-    return chordSequence;
-}
-
-makeChordSequence = function(chordNames)
-{
-    var chordSequence = [];
-    chordNames.forEach(function(chordName)
-    {
-        chordSequence.push(new Chord(notesfromchordname(chordName)));
-    });
-
-    return chordSequence;
 }

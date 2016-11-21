8d3a36f78e6a41cf08cb66de239b912707e8c543
mre <mre@ableton.com>
2016-11-21 07:43:16 +0100

    checkin datafile progressions

---
+++ js/data/mn-progressions.hpp
@@ -0,0 +1,32 @@
+{
+  {
+    mode: "dorian",
+    progression: "1,2,1,5,5,2,2,4,5,2",
+    origin: "white stripes - little apple blossom"
+  },
+  {
+    mode: "dorian",
+    progression: "1,7,4,4",
+    origin: "depeche mode - never let me down"
+  },
+  {
+    mode: "minor",
+    progression: "1,7,4,6",
+    origin: "cure - the figurehead"
+  },
+  {
+    mode: "minor",
+    progression: "1,3,6,4",
+    origin: "??"
+  },
+  {
+    mode: "minor",
+    progression: "1,4,6,4",
+    origin: "??"
+  },
+  {
+    mode: "harmonic_minor",
+    progression: "1,4,6,5",
+    origin: "amy whinehouse - back to black"
+  }
+}

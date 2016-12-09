f061e4cdf2dfab904ecd0816643e553faa17b4ca
mre <mre@ableton.com>
2016-12-09 06:51:22 +0100

    wip

---
+++ js/players/mn-sequencer-renderer.js
@@ -0,0 +1,10 @@
+
+// renders a serie of note events to be played from
+// a base array sequence in the form:
+//   { position: "1.1.1", notes: [1,3] }
+// and a harmonic structure
+
+renderSequence = function(harmonicStructure, baseSequence)
+{
+
+}

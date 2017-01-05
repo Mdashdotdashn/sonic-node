c63bbc8204aa8f529a0d4dc262545ac524cc6171
mre <mre@ableton.com>
2017-01-05 20:51:55 +0100

    Implement timeline compact / expand

--- js/sequencing/mn-beat-time-line.js
+++ js/sequencing/mn-beat-time-line.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 
 kTicksPerBeats = 24; // Equivalent to midi clock
 
@@ -22,11 +23,16 @@ Signature = function()
 }
 
 
-//------------ Time line ---------------------------
+//------------ Time line --------------------------------
+// Used to remember elements located to a given position
+// It can have two forms:
+// 1: compacted where the sequence is a map
+// whose key is the position and value an array of items
+// 2: expanded where the sequence is an array of elements
+//    { position: , element: }
 
-Timeline = function(ticksPerBeat)
+Timeline = function()
 {
-  this.length = new SequencingPosition(ticksPerBeat);
   this.sequence = [];
 }
 
@@ -45,18 +51,67 @@ Timeline.prototype.setLength = function(length)
   this.length = length;
 }
 
+Timeline.prototype.expand = function()
+{
+  var timeline = new Timeline();
+  timeline.length = this.length;
+  timeline.sequence =  _.flatMap(this.sequence,function(item)
+        {
+          var position = item.position;
+          CHECK_TYPE(item.element,Array);
+          var expanded = item.element.map(function(e)
+          {
+            return {
+               position: position,
+                 element: e
+                };
+          })
+          return expanded;
+        });
+    return timeline;
+}
+
+Timeline.prototype.compact = function()
+{
+  var timeline = new Timeline();
+  timeline.length = this.length;
+  var ticksPerBeat = timeline.length.ticksPerBeat_;
+
+  var timemap = this.sequence.reduce(function(map,item)
+  {
+    var key = ticksFromPosition(item.position);
+    var value = item.element;
+    if (!map[key])
+    {
+      map[key] = new Array;
+    }
+    map[key].push(value);
+    return map;
+  }, {});
+
+
+  timeline.sequence = _.map(timemap, function(value, key){
+      return {
+        position: createSequencingPosition(parseInt(key), ticksPerBeat),
+        element: value.element
+      }
+  })
+  return timeline;
+}
+
 createTimeline = function(elements, positionOffset)
 {
   CHECK_TYPE(positionOffset, SequencingPosition);
   var ticksPerBeat = positionOffset.ticksPerBeat_;
 
   var current = new SequencingPosition(ticksPerBeat);
-  var timeline = new Timeline(ticksPerBeat);
+  var timeline = new Timeline();
 
   elements.forEach(function(element){
     timeline.add(element, current);
     current = addPositions(current, positionOffset);
   })
 
+  timeline.length = current;
   return timeline;
 }
---
+++ test/test-beat-time-line.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+var _ = require("lodash");
+require("../js/sequencing/sequencing.js");
+require("../js/mn-utils.js");
+
+var elements = [ [1,2],[3],[6,7]]
+var beatTime = createSequencingPosition(kTicksPerBeats, kTicksPerBeats);
+var timeline = createTimeline(elements, beatTime);
+
+var expanded = timeline.expand();
+assert.equal(expanded.sequence.length, 5);
+var compacted = expanded.compact();
+assert.deepEqual(compacted, timeline);

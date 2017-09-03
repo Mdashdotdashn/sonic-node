f8a96923e692e741722b9a929725ab565024b49c
mre <mre@ableton.com>
2017-09-03 14:18:34 +0200

    remove ChordProgressionBuilder

--- js/progression/mn-chord-progression.js
+++ js/progression/mn-chord-progression.js
@@ -1,48 +1,5 @@
 _ = require("lodash");
 tonal=require("tonal");
-// Chord progression helper object
-
-var ChordProgressionBuilder = function(scaleIntervals)
-{
-  this.scaleNotes_ = scaleIntervals;
-  this.scaleNotes_.forEach(function(note)
-    {
-      this.scaleNotes_.push(note+12);
-    }, this);
-}
-
-
-// return the chord corresponding to the nth degree in the current progression
-// n starts with 1
-
-ChordProgressionBuilder.prototype.buildNotes = function(props)
-{
-  // Rather than remembering scale notes, we could remember the type of chord
-  // And use uniformly intervalsFromChordToken rather than the current ternary switch
-
-  const n = this.scaleNotes_;
-  const baseIndex = props.base - 1;
-  var root = n[baseIndex];
-
-  const intervals =
-    (props.alteration.length != 0)
-    ? intervalsFromChordToken(props.alteration)
-    : [ n[baseIndex+2] - root, n[baseIndex+4] - n[baseIndex+2] ];
-
-  root += props.pitchOffset;
-  let context = {
-    current: root,
-    notes: [root]
-  };
-
-  intervals.reduce((context, interval) => {
-    context.current += interval;
-    context.notes.push(context.current);
-    return context;
-  }, context)
-
-  return context.notes;
-}
 
 // Returns a timeline with the intervals corresponding to the chord progression
 // i.e. convert a timeline progression { 1, 3, b5} to {{1P,3M,5P}, ...}
@@ -87,34 +44,7 @@ buildChordProgression = function(degreeProgression, scaleIntervals)
     return tonal.chord.intervals(parsed.type).map(tonal.transpose(scaleIntervals[baseInterval -1])).map(tonal.transpose(offsetTranspose));
   });
 }
-// // creates a chord progression from a list of scale degree
-// // supported format:
-// //
-// // 1,2,3: natural chord from the scale degree
-//
-// makeChordProgression = function(scaleIntervals, progression)
-// {
-//   CHECK_TYPE(progression, Timeline);
-//
-//   var builder = new ChordProgressionBuilder(scaleIntervals);
-//
-//   const degreeToNotesFn = (degree) =>
-//   {
-//     const degreeAsString = ("" + degree).trim();
-//     const degreeProperties = packDegreeProperties(degreeAsString);
-//     return  builder.buildNotes(degreeProperties);
-//   };
-//
-//   var noteProgression = new Timeline();
-//
-//   progression.sequence.forEach(function(step)
-//   {
-//     noteProgression.add(degreeToNotesFn(step.element), step.position);
-//   });
-//   noteProgression.length = progression.length;
-//   return noteProgression;
-// }
-//
+
 stringForProgression = function(progression)
 {
   var chordnameList = "";
